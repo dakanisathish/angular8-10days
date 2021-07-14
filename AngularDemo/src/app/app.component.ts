daeaa0acbe9d3b6d8daad8029e8b671e77c5b86b
dakanisathish <dakani.sathish@gmail.com>
2021-07-14 23:13:06 +0530

    upto day2 completed

---
+++ AngularDemo/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ data:number=100;
+ constructor(){
+   console.log(`new - data is ${this.data}`);
+ }
+ ngOnChanges(){
+   console.log(`ngOnChanges - data is ${this.data}`);
+ }
+ ngOnInit(){
+   console.log(`ngOnInit - data is ${this.data}`)
+ }
+}
